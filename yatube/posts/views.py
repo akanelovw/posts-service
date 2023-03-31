f2a9d8cf91be440b660cdee07f2b9e9093251102
Никита Болдырев <krul1wtd@yandex.ru>
2023-03-31 19:19:37 +0700

    update

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -119,9 +119,10 @@ def follow_index(request):
 
 @login_required
 def profile_follow(request, username):
-    author = get_object_or_404(User, username=username)
-    Follow.objects.get_or_create(user=request.user, author=author)
-    return redirect('posts:profile', username)
+    if request.user != username:
+        author = get_object_or_404(User, username=username)
+        Follow.objects.get_or_create(user=request.user, author=author)
+        return redirect('posts:profile', username)
 
 
 @login_required
